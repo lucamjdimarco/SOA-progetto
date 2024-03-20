27f8f5ba67ed43f5acf8e503f7312bbbc75ab106
lucamjdimarco <lucadm24@gmail.com>
2024-03-20 23:04:59 +0100

    test su kprobe

--- test/test.c
+++ test/test.c
@@ -2,11 +2,25 @@
 #include <linux/module.h>
 #include <linux/kprobes.h>
 
+#define PATH_MAX 256
+
 static struct kprobe kp;
 
 /* Funzione di gestione pre-intercettazione */
 static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
-    printk(KERN_INFO "Intercepted do_sys_openat2\n");
+    //printk(KERN_INFO "Intercepted do_sys_openat2\n");
+    char path[PATH_MAX];
+    const char __user *filename = (const char __user *)regs->si; // Registri che contengono il puntatore al path del file
+
+    if (filename) {
+        if (strncpy_from_user(path, filename, PATH_MAX) < 0) {
+            printk(KERN_INFO "Failed to copy filename from user space\n");
+            return 0;
+        }
+        printk(KERN_INFO "File Path: %s\n", path);
+    } else {
+        printk(KERN_INFO "No filename provided\n");
+    }
     return 0;
 }
 
