fe2f67599c0bc950b74b916973250f1522229cf6
lucamjdimarco <lucadm24@gmail.com>
2024-03-20 23:21:16 +0100

    test su kprobe

--- test/test.c
+++ test/test.c
@@ -1,8 +1,10 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kprobes.h>
+#include <linux/path.h>
+#include <linux/fs.h>
 
-#define PATH_MAX 256
+#define PATH 256
 
 static struct kprobe kp;
 
@@ -24,11 +26,11 @@ int strncmp_custom(const char *s1, const char *s2, size_t n) {
 /* Funzione di gestione pre-intercettazione */
 static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
     //printk(KERN_INFO "Intercepted do_sys_openat2\n");
-    char path[PATH_MAX];
+    char path[PATH];
     const char __user *filename = (const char __user *)regs->si; // Registri che contengono il puntatore al path del file
 
     if (filename) {
-        if (strncpy_from_user(path, filename, PATH_MAX) < 0) {
+        if (strncpy_from_user(path, filename, PATH) < 0) {
             printk(KERN_INFO "Failed to copy filename from user space\n");
             return 0;
         }
@@ -36,8 +38,24 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
         if(strncmp_custom(filename, "/run", 4) == 0) {
             return 0;
         }
-        
-        printk(KERN_INFO "File Path: %s\n", path);
+
+        if (kern_path(path, LOOKUP_FOLLOW, &path_struct)) {
+            printk(KERN_INFO "Failed to get absolute path\n");
+            return 0;
+        }
+
+        abs_path = kmalloc(PATH_MAX, GFP_KERNEL);
+        if (!abs_path) {
+            printk(KERN_INFO "Failed to allocate memory for absolute path\n");
+            return 0;
+        }
+
+        path_get(&path_struct);
+        abs_path = d_path(&path_struct, abs_path, PATH_MAX);
+        printk(KERN_INFO "Absolute File Path: %s\n", abs_path);
+        kfree(abs_path);
+
+        //printk(KERN_INFO "File Path: %s\n", path);
     } else {
         printk(KERN_INFO "No filename provided\n");
     }
