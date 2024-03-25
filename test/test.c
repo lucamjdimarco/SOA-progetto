83bca413009ffb58ff4925b9923d906badade0e6
lucamjdimarco <lucadm24@gmail.com>
2024-03-25 17:32:39 +0100

    Prova nel gestire i flag

--- test/test.c
+++ test/test.c
@@ -73,7 +73,7 @@ int strncmp_custom(const char *s1, const char *s2, size_t n) {
     return 0;
 }*/
 
-int get_full_path(int dfd, char *full_path){
+/*int get_full_path(int dfd){
     char *tmp = (char*)__get_free_page(SLAB_TEMPORARY);
 
     struct file *file = fget(dfd);
@@ -88,17 +88,55 @@ int get_full_path(int dfd, char *full_path){
     	
         //printk("error: %d\n", (int)path);
         free_page((unsigned long)tmp);
-        return 1;
+        return 2;
     }
     
     //printk(KERN_INFO "Sono alla fine\n");
 
     //printk(KERN_INFO "path: %s\n", path);
     
-    strncpy(full_path, path, PATH);
+    //strncpy(full_path, path, PATH);
 
     free_page((unsigned long)tmp);
     return 0;
+}*/
+
+int get_full_path(int fd, char *path_not_abs){
+	struct path path_struct;
+	char *tmp;
+	char *path;
+	int ret;
+    int flag=0;
+	unsigned int lookup_flags = 0;
+
+	tmp=kmalloc(1024,GFP_KERNEL);
+
+	if(tpath == NULL){
+        return 1;
+    }
+	if (!(flag & AT_SYMLINK_NOFOLLOW)){
+        lookup_flags |= LOOKUP_FOLLOW;
+    }
+
+	ret = user_path_at(fd, path_not_abs, lookup_flags, &path_struct);
+	if(ret){
+		kfree(tmp);
+		return 2;
+	}
+	
+	path = d_path(&path_struct, tmp, 1024);
+    if (IS_ERR(path)) {
+        path_put(&path_struct);
+        kfree(tmp);
+        return 3;
+    }
+    printk(KERN_INFO "Path: %s\n", path);
+	kfree(tmp);		
+    path_put(&path_struct);
+
+
+	return 0;
+
 }
 
 
@@ -112,8 +150,9 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
     int fd = (int)regs->di;
 
     char *full_path = kmalloc(PATH, GFP_KERNEL);
-    struct open_how __user *user_how = (struct open_how __user *)regs->dx;
-    struct open_how how;
+    struct open_how *how = (struct open_how *)regs->dx;
+    int ret;
+    //struct open_how how;
 
 
     if (filename) {
@@ -122,26 +161,28 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
             return 0;
         }
         
-        if(strncmp_custom(filename, "/run", 4) == 0) {
+        if(strncmp_custom(path, "/run", 4) == 0) {
             return 0;
         }
 
-        //se fd == AT_FDCWD allora il path è assoluto
-        if(fd != AT_FDCWD){
-        	
-            get_full_path(fd, full_path);
-            printk(KERN_INFO "path: %s\n", full_path);
-        } 
-
-        if (copy_from_user(&how, user_how, sizeof(how))) {
-            printk(KERN_INFO "Failed to copy open_how from user space\n");
-            return 0;
+        if(strncmp_custom(path, "/", 1) != 0) {
+            ret = get_full_path(fd, path);
+            if (ret == 1) {
+                printk(KERN_INFO "Failed to get full path\n");
+                return 0;
+            } else if (ret == 2) {
+                printk(KERN_INFO "Failed to get full path\n");
+                return 0;
+            } else {
+                printk(KERN_INFO "Full path: %s\n", path);
+            }
         }
+        
 
         // Controlla se il file è aperto in scrittura o lettura/scrittura
-        if (how.flags & O_WRONLY) {
+        if (how->flags & O_WRONLY) {
             printk(KERN_INFO "File opened in write-only mode.\n");
-        } else if (how.flags & O_RDWR) {
+        } else if (how->flags & O_RDWR) {
             printk(KERN_INFO "File opened in read-write mode.\n");
         }
         
