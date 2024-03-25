df14503c76a4207d901610e17b8668bf88efb7bf
lucamjdimarco <lucadm24@gmail.com>
2024-03-25 17:49:34 +0100

    Prova nel gestire i flag

--- test/test.c
+++ test/test.c
@@ -111,7 +111,7 @@ int get_full_path(int fd, char *path_not_abs){
 
 	tmp=kmalloc(1024,GFP_KERNEL);
 
-	if(tpath == NULL){
+	if(tmp == NULL){
         return 1;
     }
 	if (!(flag & AT_SYMLINK_NOFOLLOW)){
@@ -145,17 +145,23 @@ int get_full_path(int fd, char *path_not_abs){
 static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
     //printk(KERN_INFO "Intercepted do_sys_openat2\n");
     char path[PATH];
-    const char __user *filename = (const char __user *)regs->si; // Registri che contengono il puntatore al path del file
+    //const char __user *filename = (const char __user *)regs->si; // Registri che contengono il puntatore al path del file
 
-    int fd = (int)regs->di;
 
-    char *full_path = kmalloc(PATH, GFP_KERNEL);
+    int fd = (int)(regs->di);
+    const char __user *user = ((struct filename *)(regs->si))->uptr;
+    const char __user *kernel = ((struct filename *)(regs->si))->name;
     struct open_how *how = (struct open_how *)regs->dx;
-    int ret;
-    //struct open_how how;
+
+    //printk(KERN_INFO "fd: %d\n", fd);
+    printk(KERN_INFO "user: %d\n", user);
+    printk(KERN_INFO "kernel: %d\n", kernel);
+
+    //char *full_path = kmalloc(PATH, GFP_KERNEL);
+    //int ret;
 
 
-    if (filename) {
+    /*if (filename) {
         if (strncpy_from_user(path, filename, PATH) < 0) {
             printk(KERN_INFO "Failed to copy filename from user space\n");
             return 0;
@@ -188,7 +194,11 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
         
     } else {
         printk(KERN_INFO "No filename provided\n");
-    }
+    }*/
+
+
+
+
     return 0;
 }
 
