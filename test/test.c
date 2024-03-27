1d8043e2089ce606572215cfffdc74667d3a2293
lucamjdimarco <lucadm24@gmail.com>
2024-03-27 12:14:44 +0100

    krpobe sulla do_filp_open

--- test/test.c
+++ test/test.c
@@ -18,6 +18,7 @@
 
 //krpobe struct
 static struct kprobe kp_openat2;
+static struct kprobe kp_filp_open;
 
 int strncmp_custom(const char *s1, const char *s2, size_t n) {
     size_t i;
@@ -108,21 +109,58 @@ static int handler_openat2(struct kprobe *p, struct pt_regs *regs) {
     return 0;
 }
 
+static int handler_filp_open(struct kprobe *p, struct pt_regs *regs) {
+    
+    int fd = (int)regs->di;
+    //struct filename *filename = (struct filename *)regs->si;
+    const struct open_flags *op = (const struct open_flags *)regs->dx;
+
+    const __user char *path_user = ((struct filename *)(regs->si))->uptr;
+	const char *path_kernel = ((struct filename *)(regs->si))->name;
+
+    if(path_user == NULL) {
+        printk(KERN_INFO "No path user provided\n");
+        return 0;
+    
+    } else {
+        printk(KERN_INFO "Path user: %s\n", path_user);
+    }
+    if(path_kernel == NULL) {
+        printk(KERN_INFO "No path kernel provided\n");
+        return 0;
+    } else {
+        printk(KERN_INFO "Path kernel: %s\n", path_kernel);
+    
+    }
+
+
+}
+
 static int __init kprobe_init(void) {
     kp_openat2.pre_handler = handler_openat2;
     kp_openat2.symbol_name = "do_sys_openat2"; // Nome della funzione da intercettare
+
+    kp_filp_open.pre_handler = handler_filp_open;
+    kp_filp_open.symbol_name = "do_filp_open";
 
     if (register_kprobe(&kp_openat2) < 0) {
         printk(KERN_INFO "Failed to register kprobe openat2\n");
         return -1;
     }
+    if (register_kprobe(&kp_filp_open) < 0) {
+        printk(KERN_INFO "Failed to register kprobe filp_open\n");
+        return -1;
+    }
     printk(KERN_INFO "Kprobe openat2 registered successfully\n");
+    printk(KERN_INFO "Kprobe filp_open registered successfully\n");
     return 0;
 }
 
 static void __exit kprobe_exit(void) {
     unregister_kprobe(&kp_openat2);
+    unregister_kprobe(&kp_filp_open);
     printk(KERN_INFO "Kprobe openat2 unregistered\n");
+    printk(KERN_INFO "Kprobe filp_open unregistered\n");
 }
 
 module_init(kprobe_init);
